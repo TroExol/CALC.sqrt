df3bb2abd132e6d0e148056832e94a0f2fdbb9fa
troexol <vanyavanya0707@mail.ru>
2019-09-04 23:51:53 +0500

    upd

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const jsminify = require('gulp-minify');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+const filesize = require('gulp-filesize');
+const babel = require('gulp-babel');
+
+gulp.task('clean-js', () => gulp.src('dist/js/*.min.js', {read: false}).pipe(clean()));
+gulp.task('clean-css', () => gulp.src('dist/design/css/*.min.css', {read: false}).pipe(clean()));
+
+gulp.task(
+	'css-minify',
+	gulp.series('clean-css', () =>
+		gulp
+			.src('src/design/css/*.css')
+			.pipe(filesize())
+			.pipe(
+				autoprefixer({
+					browsers: ['ie >= 8', 'last 4 version'],
+					cascade: false
+				})
+			)
+			.pipe(cssnano())
+			.pipe(rename({suffix: '.min'}))
+			.pipe(gulp.dest('dist/design/css/'))
+			.pipe(filesize())
+	)
+);
+
+gulp.task(
+	'js-minify',
+	gulp.series('clean-js', () =>
+		gulp
+			.src('src/js/*.js')
+			.pipe(filesize())
+			.pipe(babel({
+				presets: ['@babel/env']
+			}))
+			.pipe(
+				jsminify({
+					ext: {
+						min: '.min.js'
+					},
+					noSource: true
+				})
+			)
+			.pipe(gulp.dest('dist/js/'))
+			.pipe(filesize())
+	)
+);
+
+gulp.task('build', gulp.parallel('js-minify', 'css-minify'));
