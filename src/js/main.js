fc30add0930d1b127ed380af3254c6c2576f76a2
troexol <vanyavanya0707@mail.ru>
2019-09-06 03:45:41 +0500

    Added rounding setting

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.body.dataset.theme = localStorage.getItem('tr_theme') || 'light';
 });
 
-const root = (expression) => {
+const root = (expression, round = 3) => {
 	let result = 'Ваше выражение имеет ошибку или в данный момент мы не можем его решить';
 	try {
 		const evaluate = math.evaluate(expression);
 		// Извлекаем корень и округляем
-		result = math.round(math.sqrt(evaluate), 3).toString();
-
+		result = math.round(math.sqrt(evaluate), round).toString();
+		// Добавляем 2 варианта для комплексных чисел
 		if (!math.hasNumericValue(result) && /i/.test(result)) {
 			result = '+-(' + result + ')';
 		}
@@ -29,8 +29,10 @@ document.querySelector('.header-settings-theme').addEventListener('click', (even
 });
 
 document.getElementById('calc').addEventListener('click', () => {
+	let round = document.getElementById('round').value;
+	round = (/^\d$/).test(round) ? round : 3;
 	const expr = document.getElementById('expression').value;
-	const result = root(expr);
+	const result = root(expr, round);
 
 	const output = document.getElementById('output');
 	output.innerHTML = result;
