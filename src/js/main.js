20a7acb08f96649888687c1e545bdbf42e454ffb
troexol <vanyavanya0707@mail.ru>
2019-09-06 23:40:51 +0500

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,17 @@
 const languages = {
 	ru: {
 		title: 'Извлечение корня',
-		round_label: 'Чисел после запятой',
+		round_label: 'Цифр после запятой',
 		round_btn: 'Извлечь',
 		output_label: 'Корень введенного выражения равен:',
 		person_title: 'Наши разработчики',
 		klochkovskiy: 'Клочковский Иван',
 		ilkaev: 'Илькаев Артур',
 		otroshenko: 'Отрощенко Владимир',
-		contact_title: 'Связь с нами'
+		contact_title: 'Связь с нами',
+		documentation_title: 'Функционал',
+		main_title: 'Главная',
+		error: 'Ваше выражение имеет ошибку или в данный момент мы не можем его решить'
 	},
 	en: {
 		title: 'Root extraction',
@@ -19,7 +22,10 @@ const languages = {
 		klochkovskiy: 'Klochkovskiy Ivan',
 		ilkaev: 'Ilkaev Arthur',
 		otroshenko: 'Otroshenko Vladimir',
-		contact_title: 'Contact with us'
+		contact_title: 'Contact with us',
+		documentation_title: 'Functional',
+		main_title: 'Main',
+		error: 'Your expression has an error or we cannot solve it at the moment'
 	},
 	zh: {
 		title: '根拔除',
@@ -30,7 +36,10 @@ const languages = {
 		klochkovskiy: 'Klochkovskiy Ivan',
 		ilkaev: 'Ilkaev Arthur',
 		otroshenko: 'Otroshenko Vladimir',
-		contact_title: '聯繫我們'
+		contact_title: '聯繫我們',
+		documentation_title: '實用',
+		main_title: '主',
+		error: '您的表達式有錯誤，或者我們目前無法解決'
 	},
 	es: {
 		title: 'Extracción de raíces',
@@ -41,10 +50,13 @@ const languages = {
 		klochkovskiy: 'Klochkovskiy Ivan',
 		ilkaev: 'Ilkaev Arthur',
 		otroshenko: 'Otroshenko Vladimir',
-		contact_title: 'Contacta con nosotros'
+		contact_title: 'Contacta con nosotros',
+		documentation_title: 'Funcional',
+		main_title: 'Inicio',
+		error: 'Su expresión tiene un error o no podemos resolverlo en este momento'
 	}
 };
-
+const path = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
 document.addEventListener('DOMContentLoaded', () => {
 	document.body.dataset.theme = localStorage.getItem('tr_theme') || 'dark';
 	const lang = localStorage.getItem('tr_lang') || 'ru';
@@ -53,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const root = (expression, round = 3) => {
-	let result = 'Ваше выражение имеет ошибку или в данный момент мы не можем его решить';
+	let result = languages[document.documentElement.lang].error;
 	try {
 		const evaluate = math.evaluate(expression);
 		// Извлекаем корень и округляем
@@ -81,9 +93,35 @@ document.querySelector('.header-settings-theme').addEventListener('click', (even
 const changeLanguage = (lang) => {
 	document.querySelector('title').innerText = languages[lang].title;
 	document.querySelector('.header__title').innerText = languages[lang].title;
-	document.querySelector('.content-calculator__label').innerText = languages[lang].round_label;
-	document.querySelector('.content-calculator__calc').innerText = languages[lang].round_btn;
-	document.querySelector('.content-calculator-output__label').innerText = languages[lang].output_label;
+	switch (path) {
+		case 'index.html':
+			document.querySelector('.header__action[data-url="documentation"]').innerText =
+				languages[lang].documentation_title;
+			document.querySelector('.content-calculator__label').innerText = languages[lang].round_label;
+			document.querySelector('.content-calculator__calc').innerText = languages[lang].round_btn;
+			document.querySelector('.content-calculator-output__label').innerText = languages[lang].output_label;
+
+			if (/[\D]{6,}/.test(document.querySelector('.content-calculator-output__result').innerText)) {
+				document.querySelector('.content-calculator-output__result').innerText = languages[lang].error;
+			}
+			break;
+		case 'documentation.html':
+			document.querySelector('.header__action[data-url="main"]').innerText = languages[lang].main_title;
+			break;
+	}
+	if (path === 'index.html') {
+		document.getElementById('calc').addEventListener('click', () => {
+			let round = document.getElementById('round').value;
+			if (/\D/.test(round)) {
+				document.getElementById('round').value = 3;
+				round = 3;
+			}
+			const expr = document.getElementById('expression').value;
+
+			document.getElementById('output').innerHTML = root(expr, round);
+			document.querySelector('.content-calculator-output').style.display = 'block';
+		});
+	}
 	document.querySelector('.footer-team__title').innerText = languages[lang].person_title;
 	document.querySelector('.footer-team-cards-person[data-person="klochkovskiy"] .footer-team-cards-person__img').alt =
 		languages[lang].klochkovskiy;
@@ -92,9 +130,8 @@ const changeLanguage = (lang) => {
 	).innerText = languages[lang].klochkovskiy;
 	document.querySelector('.footer-team-cards-person[data-person="ilkaev"] .footer-team-cards-person__img').alt =
 		languages[lang].ilkaev;
-	document.querySelector(
-		'.footer-team-cards-person[data-person="ilkaev"] .footer-team-cards-person__name'
-	).innerText = languages[lang].ilkaev;
+	document.querySelector('.footer-team-cards-person[data-person="ilkaev"] .footer-team-cards-person__name').innerText =
+		languages[lang].ilkaev;
 	document.querySelector('.footer-team-cards-person[data-person="otroshenko"] .footer-team-cards-person__img').alt =
 		languages[lang].otroshenko;
 	document.querySelector(
@@ -111,15 +148,4 @@ document.querySelector('.header-settings-language').addEventListener('click', (e
 
 		localStorage.setItem('tr_lang', target.dataset.lang);
 	}
-});
-
-document.getElementById('calc').addEventListener('click', () => {
-	let round = document.getElementById('round').value;
-	round = /^\d$/.test(round) ? round : 3;
-	const expr = document.getElementById('expression').value;
-	const result = root(expr, round);
-
-	const output = document.getElementById('output');
-	output.innerHTML = result;
-	document.querySelector('.content-calculator-output').style.display = 'block';
 });
