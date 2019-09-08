23d7774bef23124c00da8738786937b5015adce3
troexol <vanyavanya0707@mail.ru>
2019-09-08 17:31:36 +0500

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,29 @@ const languages = {
 		contact_title: 'Связь с нами',
 		documentation_title: 'Функционал',
 		main_title: 'Главная',
+		im: 'Комплексные числа',
+		natural: 'Натуральные числа',
+		negative: 'Отрицательные числа',
+		fractional: 'Дробные числа',
+		plus: 'Сложение',
+		minus: 'Вычитание',
+		multi: 'Умножение',
+		div: 'Деление',
+		sin: 'Синус числа x',
+		cos: 'Косинус числа x',
+		tag: 'Тангенс числа x',
+		cot: 'Котангенс числа x',
+		asin: 'Арксинус числа x',
+		acos: 'Арккосинус числа x',
+		atan: 'Арктангенс числа x',
+		acot: 'Арккотангенс числа x',
+		exp: 'Экспонента числа x',
+		sqrt: 'Корень числа x',
+		pow: 'Возведение числа x в степень a',
+		log10: 'Десятичный логарифм числа x',
+		log2: 'Двоичный логарифм числа x',
+		abs: 'Модуль числа x',
+		nested: 'Вложенные функции',
 		error: 'Ваше выражение имеет ошибку или в данный момент мы не можем его решить'
 	},
 	en: {
@@ -28,6 +51,29 @@ const languages = {
 		contact_title: 'Contact with us',
 		documentation_title: 'Functional',
 		main_title: 'Main',
+		im: 'Complex numbers',
+		natural: 'Natural numbers',
+		negative: 'Division',
+		fractional: 'Fractional numbers',
+		plus: 'Addition',
+		minus: 'Subtraction',
+		multi: 'Умножение',
+		div: 'Multiplication',
+		sin: 'Sine of x',
+		cos: 'Cosine of x',
+		tag: 'The tangent of x',
+		cot: 'Cotangent of x',
+		asin: 'The arcsine of x',
+		acos: 'Arccosine of x',
+		atan: 'Arc tangent of x',
+		acot: 'Arcotangent of x',
+		exp: 'Exponent of x',
+		sqrt: 'Root of x',
+		pow: 'Raising the number x to the power of a',
+		log10: 'The decimal logarithm of x',
+		log2: 'The binary logarithm of x',
+		abs: 'The modulus of x',
+		nested: 'Nested Functions',
 		error: 'Your expression has an error or we cannot solve it at the moment'
 	},
 	zh: {
@@ -43,6 +89,29 @@ const languages = {
 		contact_title: '聯繫我們',
 		documentation_title: '實用',
 		main_title: '主',
+		im: '複數',
+		natural: '自然數',
+		negative: '負數',
+		fractional: '分數',
+		plus: '加法',
+		minus: '減法',
+		multi: '乘法',
+		div: '師',
+		sin: 'x的正弦',
+		cos: 'x的餘弦',
+		tag: 'x的正切',
+		cot: 'x的餘切',
+		asin: 'x的反正弦',
+		acos: 'x的反餘弦',
+		atan: 'x的反正切',
+		acot: 'x的鈍化',
+		exp: 'x的指數',
+		sqrt: '提取x的根',
+		pow: '將數字x增加到a的冪',
+		log10: 'x的十進制對數',
+		log2: 'x的二進制對數',
+		abs: 'x的模數',
+		nested: '嵌套函數',
 		error: '您的表達式有錯誤，或者我們目前無法解決'
 	},
 	es: {
@@ -58,6 +127,29 @@ const languages = {
 		contact_title: 'Contacta con nosotros',
 		documentation_title: 'Funcional',
 		main_title: 'Inicio',
+		im: 'Números complejos',
+		natural: 'Números naturales',
+		negative: 'Números negativos',
+		fractional: 'Números fraccionales',
+		plus: 'Además',
+		minus: 'Resta',
+		multi: 'Multiplicación',
+		div: 'División',
+		sin: 'Seno de x',
+		cos: 'Coseno de x',
+		tag: 'La tangente de x',
+		cot: 'Cotangente de x',
+		asin: 'El arcoseno de x',
+		acos: 'Arccosina de x',
+		atan: 'Arco tangente de x',
+		acot: 'Arcotangente de x',
+		exp: 'Exponente de x',
+		sqrt: 'Raíz X',
+		pow: 'Elevar el número x a la potencia de un',
+		log10: 'El logaritmo decimal de x',
+		log2: 'El logaritmo binario de x',
+		abs: 'El módulo de x',
+		nested: 'Funciones anidadas',
 		error: 'Su expresión tiene un error o no podemos resolverlo en este momento'
 	}
 };
@@ -80,7 +172,7 @@ const changeLanguage = (lang) => {
 	};
 
 	assign('title', 'innerText', 'title');
-	assign('.header__title', 'innerText', 'expression_label');
+	assign('.header__title', 'innerText', 'title');
 	assign('.footer-team__title', 'innerText', 'person_title');
 	assign(
 		'.footer-team-cards-person[data-person="klochkovskiy"] .footer-team-cards-person__name',
@@ -111,6 +203,30 @@ const changeLanguage = (lang) => {
 		// Страница с пользовательским руководством
 		case 'documentation.html':
 			assign('.header__action[data-url="main"]', 'innerText', 'main_title');
+			assign('.documentation__title', 'innerText', 'documentation_title');
+			assign('td[data-func="im"]', 'innerText', 'im');
+			assign('td[data-func="natural"]', 'innerText', 'natural');
+			assign('td[data-func="negative"]', 'innerText', 'negative');
+			assign('td[data-func="fractional"]', 'innerText', 'fractional');
+			assign('td[data-func="plus"]', 'innerText', 'plus');
+			assign('td[data-func="minus"]', 'innerText', 'minus');
+			assign('td[data-func="multi"]', 'innerText', 'multi');
+			assign('td[data-func="div"]', 'innerText', 'div');
+			assign('td[data-func="sin"]', 'innerText', 'sin');
+			assign('td[data-func="cos"]', 'innerText', 'cos');
+			assign('td[data-func="tag"]', 'innerText', 'tag');
+			assign('td[data-func="cot"]', 'innerText', 'cot');
+			assign('td[data-func="asin"]', 'innerText', 'asin');
+			assign('td[data-func="acos"]', 'innerText', 'acos');
+			assign('td[data-func="atan"]', 'innerText', 'atan');
+			assign('td[data-func="acot"]', 'innerText', 'acot');
+			assign('td[data-func="exp"]', 'innerText', 'exp');
+			assign('td[data-func="sqrt"]', 'innerText', 'sqrt');
+			assign('td[data-func="pow"]', 'innerText', 'pow');
+			assign('td[data-func="log10"]', 'innerText', 'log10');
+			assign('td[data-func="log2"]', 'innerText', 'log2');
+			assign('td[data-func="abs"]', 'innerText', 'abs');
+			assign('td[data-func="nested"]', 'innerText', 'nested');
 			break;
 	}
 };
